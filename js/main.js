3b52f0e583fb0486b938e030a8d09bd278afb30d
csdorman <csdorman@gmail.com>
2021-11-26 14:25:45 -0500

    Intro to numbers done

--- js/main.js
+++ js/main.js
@@ -2,8 +2,15 @@
 
 var hello = "Hello World";
 
+var value = 3;
+//Note that value2 is a different color since its a string (not a number)
+var value2 = "3"
+
 //To print things on the screen, use alert
 //alert(hello);
+//alert(value);
 
 //Or you can console.log it
 console.log(hello);
+console.log(value)
+console.log(value2)
