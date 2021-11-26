b690b2c1201c696734f1c8186c735eec85427eec
csdorman <csdorman@gmail.com>
2021-11-26 14:56:25 -0500

    Conditionals (pt 2).

--- js/main.js
+++ js/main.js
@@ -27,3 +27,24 @@ var open = true;
 console.log(true); //true
 console.log(!true); //false
 console.log(value == 3) //true
+
+//Conditionals/If statements
+var name = "Colin";
+//Run something if the conditional is true
+if (name == "Colin"){
+    console.log(name + " is cool.")
+}
+//Run something if the conditional is false
+if (name !== "John") {
+    console.log(name + " is cool.")
+}
+/* If value is >= 3, I'm cool.
+If value is <= 0, you're cool.
+If value is neither, noone is cool */
+if (value >= 3) {
+    console.log(name + " is really cool!")
+} else if (value <= 0 ) {
+    console.log("You are cool")
+} else {
+    console.log("Sorry. Try again")
+}
